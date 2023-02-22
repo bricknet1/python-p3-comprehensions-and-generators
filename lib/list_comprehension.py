661805c7036da7def4002618ad6efa9ff0dd6d80
Nick Johnson <nlj222@gmail.com>
2023-02-21 20:06:00 -0800

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if (num/2).is_integer()]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    bangs = [each + "!" for each in sentence_list]
+    return bangs
